12c95fafb6ef343e35ca9fff346f8d08eed6cbe4
Natalie A <nmanderson314@gmail.com>
2018-12-14 21:22:39 -0500

    remove

--- models/users.js
+++ models/users.js
@@ -6,37 +6,13 @@ module.exports = function(sequelize, DataTypes) {
       autoIncrement: true,
       primaryKey: true
     },
-<<<<<<< HEAD
-    username: {
-      type: DataTypes.STRING,
-    },
-    name: {
-      type: DataTypes.STRING,
-    },
-    email: {
-      type: DataTypes.STRING,
-    },
-    bio:{
-=======
     USERNAME: {
       type: DataTypes.STRING
     },
     PASSWORD:{
->>>>>>> 75a1ee8deb100c28c069c500ba630c443bd63423
       type: DataTypes.STRING
     }
   });
 
-<<<<<<< HEAD
-  // Users.associate = function(models) {
-  //   // Associating Users with photos
-  //   // When an Users is deleted, also delete any associated photos
-  //   Users.hasMany(models.Photos);
-  // };
-
-  return Users;
-};
-=======
   return User;
 };
->>>>>>> 75a1ee8deb100c28c069c500ba630c443bd63423
